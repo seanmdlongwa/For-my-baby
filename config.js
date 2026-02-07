6fe76b5f9490f7e5673d91cffcabe423d0036604
seanmdlongwa <u21717835@tuks.co.za>
2026-02-07 21:43:25 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Luyanda, my baby,",
+    valentineName: "Luyanda, my baby",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
